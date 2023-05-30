753bda294c2cdef5423300c78c09ca261d3803d4
EmaVillegas <villegasemanuel23@gmail.com>
2023-05-30 14:01:24 -0300

    form completo

--- index.js
+++ index.js
@@ -53,5 +53,40 @@ buttonOk.addEventListener('click',() => {
 
 
 
+// log in
+
+const login = document.querySelector('.log-in');
+const formPrincipal = document.querySelector('.container-form-principal');
+console.log(login);
+
+login.addEventListener('click',() => {
+    formPrincipal.style.display = 'block';
+});
 
 
+const closeImage = document.querySelector('.img-close');
+console.log(closeImage);
+
+closeImage.addEventListener('click', ()=>{
+    formPrincipal.style.display = 'none';
+})
+
+
+// number counter
+const backgroundColor = document.querySelector('#background-number');
+const numberCounter = document.querySelector('#number-data');
+let cantidad = 0;
+
+let tiempo = setInterval(()=>{
+    cantidad = cantidad + 1;
+    backgroundColor.style.height = `${cantidad}%`;
+    numberCounter.textContent = `+ ${cantidad}`;
+
+
+    if(cantidad === 300)
+    {
+        clearInterval(tiempo);
+    }
+ 
+
+},15);
